0e0743471ac3b6576fc89697b5bc45f810caa274
吴迪奥 <406714601@qq.com>
2020-04-06 20:27:04 +0800

    链接模型页面和主页面
    把模型页面的回调函数变为初始化函数

--- Dash App/apps/model.py
+++ Dash App/apps/model.py
@@ -1,9 +1,6 @@
 import pickle
 import copy
 import pathlib
-import dash
-import math
-import datetime as dt
 import pandas as pd
 from dash.dependencies import Input, Output, State, ClientsideFunction
 import dash_core_components as dcc
@@ -14,17 +11,15 @@ import dash_table
 from datetime import datetime
 # # Multi-dropdown options
 import plotly.graph_objects as go
-#
-# # get relative data folder
-PATH = pathlib.Path(__file__).parent
+PATH = pathlib.Path(__file__).parent.parent
 DATA_PATH = PATH.joinpath("data").resolve()
 layout = dict(
     autosize=True,
     automargin=True,
     margin=dict(l=30, r=30, b=20, t=40),
     hovermode="closest",
-    plot_bgcolor="#F9F9F9",
-    paper_bgcolor="#F9F9F9",
+    # plot_bgcolor="#F9F9F9",
+    # paper_bgcolor="#F9F9F9",
     legend=dict(font=dict(size=10), orientation="h"),
     title="Satellite Overview",
 
@@ -66,8 +61,90 @@ id_to_name_dict['128'] = '蜥蜴绝手'
 for i in match_log.columns:
     if(i.startswith("hero")):
         match_log[i] = match_log[i].apply(lambda x:id_to_name_dict[str(int(x))])
+print(match_log)
 
-app.layout = html.Div(
+
+def get_match_data():
+    data = match_log.to_dict('records')
+    return data
+
+def make_TPFN_figure():
+    TPFN_layout = copy.deepcopy(layout)
+    TPFN_layout['title'] = '分类结果混淆矩阵'
+    data = [
+        dict(
+            x=["T", "F"],
+            y = ["P","N"],
+            z = [[TP,TN],[FP,FN]],
+            type = 'heatmap',
+            colorscale='Viridis'
+        )
+    ]
+    figure = dict(data=data,layout = TPFN_layout)
+    return figure
+
+def make_accuracy_figure():
+    count_layout = copy.deepcopy(layout)
+    count_layout['title'] = '历史模型准确率'
+    x = model_list['train_ymd'].values.tolist()
+    acc_60 = model_list['accuracy_60'].values.tolist()
+    acc_75 = model_list['accuracy_75'].values.tolist()
+    acc_80 = model_list['accuracy_80'].values.tolist()
+    data = [
+        dict(
+            x = x,
+            y = acc_60,
+            type = 'line',
+            mode='lines+markers',
+            name='>60% 准确率'
+        ),
+        dict(
+            x=x,
+            y=acc_75,
+            type='line',
+            mode='lines+markers',
+            name='>75% 准确率'
+        ),
+        dict(
+            x = x,
+            y = acc_80,
+            type = 'line',
+            mode='lines+markers',
+            name='>80% 准确率'
+        ),
+    ]
+
+
+    fig = dict(data = data,layout=count_layout)
+
+    return fig
+
+def make_loss_figure():
+    loss_layout = copy.deepcopy(layout)
+    loss_layout['title'] = '训练损失函数'
+    data = [
+        dict(
+            type = 'line',
+            x = epoch,
+            y = train_loss,
+            name = '训练损失',
+            mode='lines',
+            line_shape='spline'
+        ),
+        dict(
+            type='line',
+            x=epoch,
+            y=val_loss,
+            name='交叉验证损失',
+            mode='lines',
+            line_shape='spline'
+        ),
+
+    ]
+    figure = dict(data = data,layout=loss_layout)
+    return figure
+
+model_layout = html.Div(
     [
         # empty Div to trigger javascript file for graph resizing
         html.Div(
@@ -95,7 +172,7 @@ app.layout = html.Div(
         html.Div(
             [
                 html.Div(
-                    [dcc.Graph(id='loss')],
+                    [dcc.Graph(id='loss',figure=make_loss_figure())],
                     className="pretty_container four columns",
                     id="cross-filter-options",
                 ),
@@ -133,6 +210,7 @@ app.layout = html.Div(
                             dash_table.DataTable(
                                 id = 'match_data',
                                 columns = [{"name":i,"id":i} for i in match_log.columns],
+                                data = get_match_data()
                             ),
                             id="countGraphContainer",
                             className="pretty_container",
@@ -147,12 +225,12 @@ app.layout = html.Div(
         html.Div(
             [
                 html.Div(
-                    [dcc.Graph(id="main_graph")],
+                    [dcc.Graph(id="main_graph",figure=make_TPFN_figure())],
                     className="pretty_container five columns",
                 )
                 ,
                 html.Div(
-                    [dcc.Graph(id="count_graph")],
+                    [dcc.Graph(id="count_graph",figure=make_accuracy_figure())],
                     className="pretty_container seven columns",
                 ),
             ],
@@ -177,101 +255,31 @@ app.layout = html.Div(
     style={"display": "flex", "flex-direction": "column"},
 )
 
-@app.callback(
-    Output("loss","figure"),
-    [Input("model_update_time","value")]
-)
-def make_loss_figure(update_ymd):
-    loss_layout = copy.deepcopy(layout)
-    loss_layout['title'] = '训练损失函数'
-    data = [
-        dict(
-            type = 'line',
-            x = epoch,
-            y = train_loss,
-            name = '训练损失',
-            mode='lines',
-            line_shape='spline'
-        ),
-        dict(
-            type='line',
-            x=epoch,
-            y=val_loss,
-            name='交叉验证损失',
-            mode='lines',
-            line_shape='spline'
-        ),
+# @app.callback(
+#     Output("loss","figure"),
+#     [Input("model_update_time","value")]
+# )
 
-    ]
-    figure = dict(data = data,layout=loss_layout)
-    return figure
 
-@app.callback(
-    Output("main_graph","figure"),
-    [Input("model_update_time","value")]
-)
-def make_TPFN_figure(update_ymd):
-    TPFN_layout = copy.deepcopy(layout)
-    TPFN_layout['title'] = '分类结果混淆矩阵'
-    data = [
-        dict(
-            x=["T", "F"],
-            y = ["P","N"],
-            z = [[TP,TN],[FP,FN]],
-            type = 'heatmap',
-            colorscale='Viridis'
-        )
-    ]
-    figure = dict(data=data,layout = TPFN_layout)
-    return figure
+# @app.callback(
+#     Output("main_graph","figure"),
+#     [Input("model_update_time","value")]
+# )
 
-@app.callback(
-    Output("match_data","data"),
-    [Input("model_update_time","value")]
-)
-def make_TPFN_figure(update_ymd):
-    data = match_log.to_dict('records')
-    return data
+#
+# @app.callback(
+#     Output("match_data","data"),
+#     [Input("model_update_time","value")]
+# )
+# def get_match_data(update_ymd):
+#     data = match_log.to_dict('records')
+#     return data
 
 # Main
-@app.callback(
-    Output("count_graph", "figure"),
-    [Input("model_update_time","value")]
-)
-def make_loss_figure(update_ymd):
-    count_layout = copy.deepcopy(layout)
-    count_layout['title'] = '历史模型准确率'
-    x = model_list['train_ymd'].values.tolist()
-    acc_60 = model_list['accuracy_60'].values.tolist()
-    acc_75 = model_list['accuracy_75'].values.tolist()
-    acc_80 = model_list['accuracy_80'].values.tolist()
-    data = [
-        dict(
-            x = x,
-            y = acc_60,
-            type = 'line',
-            mode='lines+markers',
-            name='>60% 准确率'
-        ),
-        dict(
-            x=x,
-            y=acc_75,
-            type='line',
-            mode='lines+markers',
-            name='>75% 准确率'
-        ),
-        dict(
-            x = x,
-            y = acc_80,
-            type = 'line',
-            mode='lines+markers',
-            name='>80% 准确率'
-        ),
-    ]
+# @app.callback(
+#     Output("count_graph", "figure"),
+#     [Input("model_update_time","value")]
+# )
 
-
-    fig = dict(data = data,layout=count_layout)
-
-    return fig
-if __name__ == "__main__":
-    app.run_server(debug=True)
+# if __name__ == "__main__":
+#     app.run_server(debug=True)
